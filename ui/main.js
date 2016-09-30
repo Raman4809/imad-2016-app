82fe6f815232840fa07691d14aeb3e7aaafc8424
Raman4809 <ratnakar.raman@gmail.com>
2016-09-30 20:06:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
 console.log('Loaded!');
+
+console.log('loaded');
+
+//Change the text of the main-text div
+var element = document.getElementById("main-text");
+
+element.innerHTML = 'New value';
+
+//move the image
+
+var img = document.getElementById('madi');
+var margin = 0;
+function moveRight (){
+	marginLeft = marginLeft + 10;
+	img.style.marginLeft = marginLeft + 'px';
+}
+img.onClick = function () {
+	var interval = setInterval(moveRight, 50);
+}
