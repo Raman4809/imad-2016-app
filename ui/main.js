1a9748cde8332161543defb08b6046c19a921b8b
Raman4809 <ratnakar.raman@gmail.com>
2016-10-12 18:27:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,12 @@ element.innerHTML = 'New Value';
 //move the image
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onClick = function(){
-    img.style.marginLeft = '100px';
-    
+    var interval = setInterval(moveRight, 100);
 };
