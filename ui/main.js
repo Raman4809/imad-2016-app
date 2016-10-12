8260ded33e96d06d095d5c4b3d799c8a931cac3a
Raman4809 <ratnakar.raman@gmail.com>
2016-10-12 23:54:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,10 +24,10 @@ button.onclick = function(){
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
+    
     //Create a request object
     var request = new XMLHttpRequest();
     
@@ -51,6 +51,8 @@ submit.onclick = function(){
     };
   
    //Make the request
+   var nameInput = document.getElementById('name');
+   var name = nameInput.value;
    request.open('GET', 'http://raman4809.imad.hasura-app.io/submit-name?name'+name, true);
    request.send(null);
 };
